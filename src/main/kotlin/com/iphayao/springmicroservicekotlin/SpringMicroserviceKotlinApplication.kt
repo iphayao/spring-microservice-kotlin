4f81f36a746ca13115b70690f6834b8c696d7e16
iphayao <phayaob@gmail.com>
2019-01-01 15:25:30 +0700

    Initialize project

---
+++ src/main/kotlin/com/iphayao/springmicroservicekotlin/SpringMicroserviceKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.iphayao.springmicroservicekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMicroserviceKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringMicroserviceKotlinApplication>(*args)
+}
+
